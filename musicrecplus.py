d1a92da93970b9017db769aa5eead04ef666fae4
ruhiajinkya <111522073+ruhiajinkya@users.noreply.github.com>
2024-11-22 15:07:00 -0500

    Add files via upload

--- musicrecplus.py
+++ musicrecplus.py
@@ -1,19 +1,20 @@
-def enterPreferences():
-    pass
+def read_preferences(filename):
+    dic = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            [username, singers] = line.strip().split(":")
+            singersList = singers.strip.split(",")
+            dic[username.rstrip()] = singersList
+    return dic
 
-def getRecommendations():
-    pass
+def write_preferences(filename):
+    
+    with open(filename, 'w') as file:
+        for line in file:
+            username = input("Enter your name (put a $ symbol after your name if you wish your preferences to remain private):")
+            singersList = input("Enter an artist that you like (Enter to finish) ")
+            file.write(username + ":" + singersList)
+    filename.close()
 
-def showMostPopularArtist():
-    pass
-
-def howPopularIsMostPopular():
-    pass
-
-def whichUserHasMostLikes():
-    pass
-
-def saveAndQuit():
-    pass
-
-# Main loop
+def showMenu():
+    print("Enter a letter to choose an option:\ne - Enter preferences\nr - Get recommendations\np= Show most popular artists\nh - How popular is the most popular\nm - Which user has the most likes\nq - Save and quit")
