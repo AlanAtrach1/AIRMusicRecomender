8e99491d229b0183ca7b3a0510dbb87d8a870eae
Ian Chao <teriyake4@outlook.com>
2024-11-22 13:37:24 -0500

    Create musicrecplus.py

---
+++ musicrecplus.py
@@ -0,0 +1,19 @@
+def enterPreferences():
+    pass
+
+def getRecommendations():
+    pass
+
+def showMostPopularArtist():
+    pass
+
+def howPopularIsMostPopular():
+    pass
+
+def whichUserHasMostLikes():
+    pass
+
+def saveAndQuit():
+    pass
+
+# Main loop
